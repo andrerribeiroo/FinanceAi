dee3696b8c109423a7132ac0a675b35bcc21cd57
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2024-11-05 22:06:49 -0300

    .

--- finance-ai/app/login/page.tsx
+++ finance-ai/app/login/page.tsx
@@ -15,26 +15,25 @@ const LoginPage = async () => {
       {/* ESQUERDA */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
-          src="logo.svg"
+          src="/logo.svg"
           width={173}
           height={39}
           alt="Finance AI"
           className="mb-8"
         />
-        <h1 className="mb-3 text-4xl font-bold">Bem vindo</h1>
-        <p className="text-muted-foreground">
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
           <Button variant="outline">
-            <LogInIcon />
+            <LogInIcon className="mr-2" />
             Fazer login ou criar conta
           </Button>
         </SignInButton>
       </div>
-
       {/* DIREITA */}
       <div className="relative h-full w-full">
         <Image
